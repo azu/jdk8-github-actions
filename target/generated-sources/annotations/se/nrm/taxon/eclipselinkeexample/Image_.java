788867151deeaf8738afefce39f05c638b1217ea
azu <azuciao@gmail.com>
2020-01-15 13:01:55 +0900

    test

---
+++ target/generated-sources/annotations/se/nrm/taxon/eclipselinkeexample/Image_.java
@@ -0,0 +1,18 @@
+package se.nrm.taxon.eclipselinkeexample;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import se.nrm.taxon.eclipselinkeexample.Licence;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2020-01-15T12:59:03")
+@StaticMetamodel(Image.class)
+public class Image_ { 
+
+    public static volatile SetAttribute<Image, Licence> licenses;
+    public static volatile SingularAttribute<Image, Long> imageId;
+    public static volatile SingularAttribute<Image, String> copyrightOwner;
+    public static volatile SingularAttribute<Image, String> title;
+
+}
